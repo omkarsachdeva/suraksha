fabff169826becd482b763d199137c60c621d275
omkarsachdeva <omkarsachdeva3832stella@gmail.com>
2024-09-05 22:16:52 +0530

    updates

--- app/src/main/java/com/whitehats/suraksha/CurrentLocation.java
+++ app/src/main/java/com/whitehats/suraksha/CurrentLocation.java
@@ -41,7 +41,7 @@ public class CurrentLocation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_main);
+        setContentView (R.layout.activity_current_location);
 
         mLatitudeLabel = "latitude";
         mLongitudeLabel = "longitude";
